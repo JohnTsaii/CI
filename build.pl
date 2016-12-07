91563343b2027011ec748031b4ff57d987aa7df1
John Tsai <caijingzhuang@gmail.com>
2016-12-07 14:30:46 +0800

    add base files

---
+++ build.pl
@@ -0,0 +1,28 @@
+# /usr/bin/perl
+
+use strict;
+use warnings;
+
+my $target = 'TargetName';
+my $workspace_name = "WorkSpaceName";
+my $project_name = "PorjectName";
+my $scheme = 'SchemeName';
+my $configuration = 'Release';
+my $project_type_option = "-project";
+my $project_type_parameter = "$project_name.xcodeproj";
+
+my $profile_name = "ProfileName";
+my $code_sign_identity = "CodeSignIdnetity";
+my $development_team = 'DevelopmentTeam';
+    
+system("xcodebuild", "$project_type_option", "$project_type_parameter", "-scheme", "$scheme", "clean");
+
+if ($? != 0) {}
+
+system("xcodebuild", "$project_type_option", "$project_type_parameter", "-scheme", "$scheme", "-configuration", "$configuration", "-sdk", "iphoneos", "PROVISIONING_PROFILE=$profile_name", "DEVELOPMENT_TEAM=$development_team", "build");
+
+if ($? != 0) {}
+system("xcodebuild", "$project_type_option", "$project_type_parameter", "-scheme", "$scheme", "-configuration", "$configuration", "-sdk", "iphoneos", "PROVISIONING_PROFILE=$profile_name", "DEVELOPMENT_TEAM=$development_team", "-archivePath", "build/$scheme.xcarchive", "archive");
+
+system("xcodebuild", "-exportArchive", "-archivePath", "build/$scheme.xcarchive", "-exportPath", "build/$scheme.ipa", "-sdk", "iphoneos","-exportOptionsPlist", "enterpriseExport.plist");
+
